450d81bba6b203096e57ed934abaaaeb0a4fe41d
Seongwon97 <osw970325@naver.com>
2021-09-09 22:45:50 +0900

    18. JUnit

---
+++ 18. JUnit/JunitTest/src/test/java/DollarControllerTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class DollarControllerTest {
+    @Test
+    public void testHello(){
+        System.out.println("hello");
+    }
+
+    @Test
+    public void dollar(){
+        MarketApi marketApi = new MarketApi();
+        DollarCalculator dollarCalculator = new DollarCalculator(marketApi);
+        dollarCalculator.init();
+
+        Calculator calculator = new Calculator(dollarCalculator);
+
+        System.out.println(calculator.sum(10, 10));
+
+        Assertions.assertEquals(22000, calculator.sum(10, 10));
+
+    }
+}
