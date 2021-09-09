ce4b6401e9b284b9f4c3d07e810c72a7d7c4aebf
Seongwon97 <osw970325@naver.com>
2021-09-09 23:27:59 +0900

    18. JUnit (Spring환경)

---
+++ 18. JUnit/JUnitTest_Spring/src/main/java/com/example/junittest_spring/component/MarketApi.java
@@ -0,0 +1,10 @@
+package com.example.junittest_spring.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+    public int connect(){
+        return 1100;
+    }
+}
---
+++ 18. JUnit/JUnitTest_Spring/src/test/java/com/example/junittest_spring/component/DollarCalculatorTest.java
@@ -0,0 +1,32 @@
+package com.example.junittest_spring.component;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+
+// SpringBootTest를 붙이는 순간 모든 Bean이 추가된다.
+@SpringBootTest
+public class DollarCalculatorTest {
+
+    @MockBean
+    private MarketApi marketApi;
+
+    @Autowired
+    private DollarCalculator dollarCalculator;
+
+    @Test
+    public void dollarCalculatorTest(){
+        Mockito.when(marketApi.connect()).thenReturn(3000);
+        dollarCalculator.init();
+
+        int sum = dollarCalculator.sum(10,10);
+        int minus = dollarCalculator.minus(10, 10);
+
+        Assertions.assertEquals(60000, sum);
+        Assertions.assertEquals(0, minus);
+    }
+}
