3509b9f2ed1289597812152963c4ba5e57822fed
Seongwon97 <osw970325@naver.com>
2021-09-14 22:44:10 +0900

    21. JPA 기초 (count, exist, delete)
    count, exist, delete

--- 21. JPA 기초/JPA_Study/src/test/java/com/example/jpa_study/repository/UserRepositoryTest.java
+++ 21. JPA 기초/JPA_Study/src/test/java/com/example/jpa_study/repository/UserRepositoryTest.java
@@ -64,5 +64,46 @@ class UserRepositoryTest {
 
         // -> findById는 eager한 patch를 하여 읽어오는 것 까지 한번에 하지만
         // getOne은 lazy한 patch를 하여 값을 읽어오지 않고 가져오기만 한다.
+
+
+
+        ////////////////////////
+        ////    flush 예제  ////
+        ///////////////////////
+//        userRepository.save(new User("Jack", "jack@gmail.com"));
+//
+//        userRepository.flush(); // query에 변화를 주는게 아니라 DB반영 시점을 조절하는 것이다. (현재 확이하기는 어려움)
+//
+//        userRepository.findAll().forEach(System.out::println);
+
+
+        ////////////////////////
+        ////    count 예제  ////
+        ///////////////////////
+//        long count = userRepository.count();
+//        System.out.println(count);
+
+
+        ////////////////////////
+        ////    exist 예제  ////
+        ///////////////////////
+//        boolean exists = userRepository.existsById(1L);
+//        System.out.println(exists);
+
+
+        ////////////////////////
+        ////   Delete 예제  ////
+        ///////////////////////
+//        userRepository.delete(userRepository.findById(3L).orElseThrow(RuntimeException::new));
+//        userRepository.deleteById(3L);
+//        userRepository.deleteAll(userRepository.findAllById(Lists.newArrayList(3L, 4L)));
+//        userRepository.deleteAll();
+//        findAll과 같이 DeleteAll도 많은 데이터가 있는 경우 성능 문제가 발생할 것이다.
+//        userRepository.deleteAllInBatch(userRepository.findAllById(Lists.newArrayList(3L, 4L)));
+//        userRepository.deleteAllInBatch();
+//        userRepository.findAll().forEach(System.out::println);
+
+//        deleteAllInBatch와 deleteAll()를 비교하였을 때 Batch를 사용하면 Delete가 1번만 호출되지만 deleteAll을 하면 데이터의 수만큼 Delete가 호출된다.
+
     }
 }
--- 21. JPA 기초/JPA_Study/src/test/resources/data.sql
+++ 21. JPA 기초/JPA_Study/src/test/resources/data.sql
@@ -24,8 +24,8 @@ insert into user(`id`, `name`, `email`, `created_at`, `updated_at`) values (5, '
 */
 
 
-insert into user (id, name, email, created_at, updated_at) values (1, 'martin', 'martin@gmail.com', now(), now());
-insert into user (id, name, email, created_at, updated_at) values (2, 'James', 'James@gmail.com', now(), now());
+insert into user (id, name, email, created_at, updated_at) values (6, 'martin', 'martin@gmail.com', now(), now());
+insert into user (id, name, email, created_at, updated_at) values (7, 'James', 'James@gmail.com', now(), now());
 insert into user (id, name, email, created_at, updated_at) values (3, 'sophia', 'sophia@gmail.com', now(), now());
 insert into user (id, name, email, created_at, updated_at) values (4, 'Lisa', 'Lisa@gmail.com', now(), now());
 insert into user (id, name, email, created_at, updated_at) values (5, 'martin', 'martin123@gmail.com', now(), now());
