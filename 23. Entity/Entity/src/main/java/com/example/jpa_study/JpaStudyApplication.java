c229d3f8eefd1f0eed5fd34ebb186495e32c36c4
Seongwon97 <osw970325@naver.com>
2021-09-18 02:06:56 +0900

    23. Entity (Entity Listener학습_JpaAuditing)
    DB에 값을 업데이트/생성함에 따라 생성/업데이트 타임을 변경해주는 annotation이 존재한다. (앞서 구현한  MyEntityListener.class와 같은 기능)
    그러한 AuditingEntityListener.class에 대한 코드들을 학습해보았다.

--- 23. Entity/Entity/src/main/java/com/example/jpa_study/JpaStudyApplication.java
+++ 23. Entity/Entity/src/main/java/com/example/jpa_study/JpaStudyApplication.java
@@ -2,8 +2,10 @@ package com.example.jpa_study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 앞서 구현한 CreatedAt, UpdatedAt의 기능은 많이 사용되어서 JPA에서 기본적으로 제공한다.
 public class JpaStudyApplication {
 
     public static void main(String[] args) {
--- 23. Entity/Entity/src/main/java/com/example/jpa_study/domain/Book.java
+++ 23. Entity/Entity/src/main/java/com/example/jpa_study/domain/Book.java
@@ -2,6 +2,9 @@ package com.example.jpa_study.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -9,7 +12,8 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Data
-@EntityListeners(value = MyEntityListener.class) // MyEntityListener.class를 Entity Listener로 설정
+@EntityListeners(value = AuditingEntityListener.class) // MyEntityListener.class와 같은 기능을 하는데 JPA에서 제공하는 기본 기능을 사용하는 방법
+// @EntityListeners(value = MyEntityListener.class) // MyEntityListener.class를 Entity Listener로 설정
 // 여러 Class에서 같은 @PrePersist와 같은 listener를 사용할 때 사용한다.
 // 공통적인 부분을 구현해두고 Listener를 annotation코드 한줄로 여러 객체에 주입 가능하다!! (반복적인 코딩을 줄일 수 있다.)
 public class Book implements Auditable{
@@ -21,9 +25,13 @@ public class Book implements Auditable{
 
     private String author;
 
+    @CreatedDate // Auditing에서 데이터가 생성될 때 업데이트 해주길 바라는 변수에 @CreatedDate를 붙인다.
     private LocalDateTime createdAt;
 
+    @LastModifiedDate // Auditing에서 데이터가 업데이트 될때 업데이트 해주길 바라는 변수에 LastModifiedDate를 붙인다.
     private LocalDateTime updatedAt;
+    // @CreatedBy, @LastModifiedBy도 연습해볼것
+
 
 //    @PrePersist
 //    public void prePersist() {
