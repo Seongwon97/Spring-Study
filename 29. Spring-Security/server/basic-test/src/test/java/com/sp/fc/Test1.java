3271aead3c869596ae14c0327e712834350b3855
Seongwon97 <osw970325@naver.com>
2021-10-24 23:26:51 +0900

    Create 29.Spring-Security

---
+++ 29. Spring-Security/server/basic-test/src/test/java/com/sp/fc/Test1.java
@@ -0,0 +1,14 @@
+package com.sp.fc;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+
+    @Test
+    void test_1() {
+        assertEquals("test", "test");
+    }
+}
