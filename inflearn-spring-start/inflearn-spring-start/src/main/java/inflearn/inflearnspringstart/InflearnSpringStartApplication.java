4dfc27e8dddcf884f1fe1deceabb502b8ed9e338
Seongwon97 <osw970325@naver.com>
2021-12-04 17:57:17 +0900

    Create Inflearn spring boot starter
    init project

---
+++ inflearn-spring-start/inflearn-spring-start/src/main/java/inflearn/inflearnspringstart/InflearnSpringStartApplication.java
@@ -0,0 +1,13 @@
+package inflearn.inflearnspringstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringStartApplication.class, args);
+	}
+
+}
