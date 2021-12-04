6aaf607a0e20392c6eb2662f1b45a26cbabdc4a5
Seongwon97 <osw970325@naver.com>
2021-12-04 23:30:18 +0900

    Update Inflearn spring boot starter
    - 홈, 등록, 조회 화면 및 기능 추가

--- inflearn-spring-start/inflearn-spring-start/src/main/java/inflearn/inflearnspringstart/controller/MemberController.java
+++ inflearn-spring-start/inflearn-spring-start/src/main/java/inflearn/inflearnspringstart/controller/MemberController.java
@@ -1,8 +1,14 @@
 package inflearn.inflearnspringstart.controller;
 
+import inflearn.inflearnspringstart.domain.Member;
 import inflearn.inflearnspringstart.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; // 다시 root로 이동
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
