c888c76d9c11457c4d2b1530b0259015b5ca7d9a
Seongwon97 <osw970325@naver.com>
2021-12-05 16:15:59 +0900

    Update Inflearn spring boot starter
    - connect H2 DB
    - setting JDBC

--- inflearn-spring-start/inflearn-spring-start/src/main/java/inflearn/inflearnspringstart/SpringConfig.java
+++ inflearn-spring-start/inflearn-spring-start/src/main/java/inflearn/inflearnspringstart/SpringConfig.java
@@ -1,21 +1,31 @@
 package inflearn.inflearnspringstart;
 
+import inflearn.inflearnspringstart.repository.JdbcMemberRepository;
 import inflearn.inflearnspringstart.repository.MemberRepository;
 import inflearn.inflearnspringstart.repository.MemoryMemberRepository;
 import inflearn.inflearnspringstart.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+// return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
